3ddc5453d1bea4f51dd116636570ab515bfa9eb6
Matheus diogenes <matheusdiogenes98@gmail.com>
2022-06-23 10:57:56 -0300

    update UserRepositoryInMemory: add method save

--- src/infra/db/UserRepoInMemory.spec.ts
+++ src/infra/db/UserRepoInMemory.spec.ts
@@ -1,9 +1,28 @@
-import { UserEntity, CreateUserInput } from "../../domain/user";
+import { CreateUserInput, UpdateUserInput } from "../../domain/user";
 import { UserRepoInMemory } from "./UserRepoInMemory";
+import { randomUUID } from 'crypto'
 
 describe('UserRepoInMemory Test', () => {
 
   it('should insert a new user', async () => {
+
+    const userRepoInMemory = new UserRepoInMemory()
+    const userData: CreateUserInput = {
+      email: 'any_email',
+      username: 'any username',
+      name: "any name",
+      password: 'secret'
+    }
+    const user = await userRepoInMemory.create(userData)
+
+    expect(user.id).toBeDefined();
+    expect(user.name).toEqual(userData.name)
+    expect(user.username).toEqual(userData.username)
+    expect(user.email).toEqual(userData.email)
+    expect(user.password).toEqual(userData.password)
+  })
+
+  it('should find a user', async () => {
     const userRepoInMemory = new UserRepoInMemory()
     const userData: CreateUserInput = {
       email: 'any_email',
@@ -12,16 +31,40 @@ describe('UserRepoInMemory Test', () => {
       password: 'secret'
     }
 
-    const user = new UserEntity(userData)
+    const user = await userRepoInMemory.create(userData)
+    const findUser = await userRepoInMemory.findOne(user.id)
 
-    await userRepoInMemory.create(user)
-
-    expect(user.id).toBeDefined();
-    expect(user.name).toEqual(userData.name)
-    expect(user.username).toEqual(userData.username)
-    expect(user.email).toEqual(userData.email)
-    expect(user.password).toEqual(userData.password)
-    expect(user.todos).toHaveLength(0)
+    expect(findUser!.id).toBeDefined();
+    expect(findUser!.name).toEqual(userData.name)
+    expect(findUser!.username).toEqual(userData.username)
+    expect(findUser!.email).toEqual(userData.email)
+    expect(findUser!.password).toEqual(userData.password)
 
   })
+
+  it('should update a user', async () => {
+    const userFake = {
+      id: randomUUID(),
+      email: 'any_email',
+      username: 'any username',
+      name: "any name",
+      password: 'secret'
+    }
+    const userRepoInMemory = new UserRepoInMemory([userFake])
+    const updateData: UpdateUserInput = {
+      email: 'new_email',
+      username: 'new username',
+      name: "new name",
+      password: 'secretkey'
+    }
+    
+    const userUpdated = await userRepoInMemory.save(userFake.id, updateData)
+
+    expect(userUpdated!.id).toBeDefined();
+    expect(userUpdated!.name).toEqual(updateData.name)
+    expect(userUpdated!.username).toEqual(updateData.username)
+    expect(userUpdated!.email).toEqual(updateData.email)
+    expect(userUpdated!.password).toEqual(updateData.password)
+  })
+
 })
--- src/infra/db/UserRepoInMemory.ts
+++ src/infra/db/UserRepoInMemory.ts
@@ -1,15 +1,38 @@
-import { CreateUserInput, IUserRepository, UpdateUserInput, UserEntity } from "../../domain/user";
+import { CreateUserInput, CreateUserOutput, IUserRepository, updateUser, UpdateUserInput, UserEntity } from "../../domain/user";
 
-export class UserRepoInMemory implements IUserRepository{
-  users: UserEntity[] = []
+export class UserRepoInMemory implements IUserRepository {
+  users: CreateUserOutput[] = []
 
-  async create(userData: CreateUserInput): Promise<UserEntity> {
-    const user = new UserEntity(userData)
-    this.users.push(user)  
+  constructor(users?: CreateUserOutput[]) {
+    this.users = users || []
+  }
+
+  async create(userData: CreateUserInput, id?: string): Promise<UserEntity> {
+    const user = new UserEntity(userData, id)
+    this.users.push(user)
     return user
   }
 
   async update(userUpdate: UpdateUserInput): Promise<void> {
     throw new Error('Not implemented')
+  }
+
+  async findOne(id: string): Promise<CreateUserOutput | undefined> {
+    const user = this.users.find(u => u.id === id)
+    if (!user) {
+      return undefined
+    }
+    return user
+  }
+
+  async save(id: string, userUpdate: UpdateUserInput): Promise<void | CreateUserOutput> {
+    const findOne = this.users.find(u => u.id === id)    
+    const user = updateUser(findOne, userUpdate)
+    if(user instanceof Error){
+      return
+    }
+    this.users = this.users.filter(u => u.id !== id)        
+    this.users.push(user)
+    return user
   }
 }
