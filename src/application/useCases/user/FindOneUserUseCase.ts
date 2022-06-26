8ee0e54ee15d0dff4ad020b7ae1bf0997b7fea41
Matheus diogenes <matheusdiogenes98@gmail.com>
2022-06-25 21:50:36 -0300

    use cases user: findOne

---
+++ src/application/useCases/user/FindOneUserUseCase.ts
@@ -0,0 +1,10 @@
+import { IUserRepository } from "../../../domain/user";
+
+export class FindOneUserUseCase {
+  constructor(private userRepo: IUserRepository) { }
+
+  async exec(id: string) {
+    const user = await this.userRepo.findOne(id)
+    return user
+  }
+}
